e70e13b614f43a4aa5aa04dfc6b09a2913739287
Mercedes <mcarballalfr@gmail.com>
2020-07-28 09:41:41 +0200

    add docs

---
+++ docs/precache-manifest.a1e828931450f73b8ba6c5efc5bf6101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15dcd803b07c5ed1cfe1cd68954848ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df0e63de87dd863f85c1",
+    "url": "./static/css/main.129f5d49.chunk.css"
+  },
+  {
+    "revision": "ba9ca757bff0978ae21b",
+    "url": "./static/js/2.ccaae067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ccaae067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0e63de87dd863f85c1",
+    "url": "./static/js/main.874fe745.chunk.js"
+  },
+  {
+    "revision": "13b0b208182c9030010e",
+    "url": "./static/js/runtime-main.787b5d83.js"
+  }
+]);
