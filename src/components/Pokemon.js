32239b0dfce80ccf6327bdd8081724171aee39f9
Mercedes <mcarballalfr@gmail.com>
2020-07-28 10:27:43 +0200

    add styles cards

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,18 +2,27 @@ import React from 'react';
 
 const Pokemon = (props) => {
   const typesLi = props.types.map((type, index) => {
-    return <li key={index}>{type}</li>;
+    return (
+      <li key={index} className='card__types__list'>
+        {type.toUpperCase()}
+      </li>
+    );
   });
   return (
     <div className='card__description'>
       <img className='card__img' src={props.url} alt='imagen' />
       <h2 className='card__name '>{props.name}</h2>
-      <h4>Evolución de:</h4>
-      <p className='card__evolution'>
-        {props.evolution === null ? 'No ha evolucionado' : props.evolution}
-      </p>
-      <h4>De tipo:</h4>
-      <ul className='card__types '>{typesLi}</ul>
+      <div className='card__evolution'>
+        <h4>Evolución de:</h4>
+        <p className='card__evolution__name'>
+          {props.evolution === null ? 'No ha evolucionado' : props.evolution}
+        </p>
+      </div>
+
+      <ul className='card__types '>
+        <h4>De tipo:</h4>
+        <div className='card__types__box'>{typesLi}</div>
+      </ul>
     </div>
   );
 };
